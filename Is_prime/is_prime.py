0d319a10d1bdb9808c7475e42e64aff538eb7573
Ancentkitonyi <ancentkitonyi.m@gmail.com>
2016-11-30 17:38:44 +0300

    is_prime function

--- Is_prime/is_prime.py
+++ Is_prime/is_prime.py
@@ -1,9 +1,12 @@
 num = int(input('Enter a number to find out if it a prime number'))
-
 if num > 1:
     #iteration within the given range
     for i in range(2,num):
         if num % i == 0:
+            print num, 'is not a prime number'
+            break
+    else:
+        print num, 'is a prime number'
 #eliminate number less or equal than one since prime number start from two
 else:
     print num, 'is not prime number'
