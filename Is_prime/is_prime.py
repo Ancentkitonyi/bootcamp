a8f214f16de1fab1e5ddb0974fe635e19d1d8eec
Ancentkitonyi <ancentkitonyi.m@gmail.com>
2016-11-30 17:21:22 +0300

    iteration over the range of the given number

--- Is_prime/is_prime.py
+++ Is_prime/is_prime.py
@@ -1,6 +1,9 @@
 num = int(input('Enter a number to find out if it a prime number'))
 
 if num > 1:
-    pass
+    #iteration within the given range
+    for i in range(2,num):
+        if num % i == 0:
+#eliminate number less or equal than one since prime number start from two
 else:
     print num, 'is not prime number'
