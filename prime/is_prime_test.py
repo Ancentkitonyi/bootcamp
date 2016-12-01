ce50b01ff1ba8f6d11e6330bc6c1de9b05a05047
Ancentkitonyi <ancentkitonyi.m@gmail.com>
2016-12-01 08:01:09 +0300

    10 unitcase

--- prime/is_prime_test.py
+++ prime/is_prime_test.py
@@ -6,6 +6,8 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(is_prime(5), 'is a prime number')
     def test_negative(self):
         self.assertEqual(is_prime(-5), False)
+    def test_zero_non_prime(self):
+        self.assertEqual(is_prime(0), False)
     def test_string(self):
         self.assertEqual(is_prime('jj'), False) 
     def test_list(self):
@@ -18,5 +20,15 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(is_prime(2), 'is a prime number')
     def test_is_prime(self):
         self.assertEqual(is_prime(1), False)
+    def test_is_prime(self):
+        self.assertEqual(is_prime(1000), False)
+    def test_not_boolen(self):
+        self.assertEqual(is_prime(True), False)
+    def test_is_7_prime(self):
+        self.assertEqual(is_prime(7), 'is a prime number')
+    def test_is_3_prime(self):
+        self.assertEqual(is_prime(3), 'is a prime number')
+    
+    
 if __name__ == '__main__':
 	unittest.main()
